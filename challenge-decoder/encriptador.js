f8bb516276dd0298dcd7cfd561afd5d8cf06c7b9
forsthuberdiego <91010648+forsthuberdiego@users.noreply.github.com>
2022-01-22 09:14:42 -0300

    Delete encriptador.js

--- challenge-decoder/encriptador.js
+++
@@ -1,6 +0,0 @@
-var textoIngreso = "hola como estas";
-
-var texto = textoIngreso;
-texto2 = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
-
-console.log(texto2)
